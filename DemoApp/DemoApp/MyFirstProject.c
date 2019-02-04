e3e3ea483e8a35548b3501296c196f46e26aa63a
Daniel <thecrazygamer747@gmail.com>
2019-02-04 15:16:57 -0800

    DemoApp Project

---
+++ DemoApp/DemoApp/MyFirstProject.c
@@ -0,0 +1,21 @@
+/********************************/
+/*  Programmer: Daniel Murdock  */
+/*     Project: C Project #     */
+/*         Demo Project         */
+/*        MoWe  CSCI 112        */
+/********************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+	// Print the words "Hello World!" on the screen
+
+	printf("Hello World!\nYippe\n");
+	printf("My name is Daniel\n");
+
+	system("pause");
+	return 0;
+}

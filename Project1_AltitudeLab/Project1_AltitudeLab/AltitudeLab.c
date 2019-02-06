6151a9937ffd96a270a92cab7a4bf6d449f76a65
Daniel <thecrazygamer747@gmail.com>
2019-02-06 15:34:56 -0800

    Creation of the Altitude Lab project

---
+++ Project1_AltitudeLab/Project1_AltitudeLab/AltitudeLab.c
@@ -0,0 +1,22 @@
+/********************************/
+/*  Programmer: Daniel Murdock  */
+/*     Project: C Project 1     */
+/*         Altitude Lab         */
+/*        MoWe  CSCI 112        */
+/********************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+	// determines how far away from an airport a pilot needs to start descending based on their altitude,
+	//speed, and assuming the standard descent rate of 500 feet per minute
+
+	int RATE = 500, alt = 0, speed = 0;
+	
+
+	system("pause");
+	return 0;
+}

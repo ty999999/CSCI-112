9e8fd7c98f933b569a45ed35e74256e2afb7f89b
Daniel <thecrazygamer747@gmail.com>
2019-02-06 17:01:09 -0800

    Template

---
+++ HeartLab.c
@@ -0,0 +1,18 @@
+/********************************/
+/*  Programmer: Daniel Murdock  */
+/*     Project: C Project #     */
+/*         Demo Project         */
+/*        MoWe  CSCI 112        */
+/********************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+	
+
+	system("pause");
+	return 0;
+}

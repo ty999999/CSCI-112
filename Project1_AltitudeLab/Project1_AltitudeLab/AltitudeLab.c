0aedaaafbef015ca60811b5181fa6f076a685e32
Daniel <thecrazygamer747@gmail.com>
2019-02-06 16:59:26 -0800

    Mostly Completed Altitude Lab

--- Project1_AltitudeLab/Project1_AltitudeLab/AltitudeLab.c
+++ Project1_AltitudeLab/Project1_AltitudeLab/AltitudeLab.c
@@ -14,7 +14,26 @@ int main() {
 	// determines how far away from an airport a pilot needs to start descending based on their altitude,
 	//speed, and assuming the standard descent rate of 500 feet per minute
 
-	int RATE = 500, alt = 0, speed = 0;
+	int RATE = 500, alt = 0, mph = 0;
+	double min = 0.0, miles = 0.0, mpm = 0.0;
+
+	//Asks the user to enter their speed and their altitude
+	printf("Please enter your speed in miles per hour: ");
+	scanf("%d", &mph);
+	printf("Please enter your altitude in feet: ");
+	scanf("%d", &alt);
+
+	//Converts the planes speed from miles per hour to miles per minute
+	mpm = (double) mph / 60;
+
+	//Determines how many minutes are needed to descend at 500 fpm
+	min = alt / RATE;
+
+	//Calculates how many miles from the airport the pilot should start descending
+	miles = min * mpm;
+
+	//Displays how far away from the airport the pilot should start descending
+	printf("You should start descending %7.3f miles from the airport.\n", miles);
 	
 
 	system("pause");
